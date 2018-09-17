c03e7675cddc326c35975e6e71000939317ab97b
martindestefanis <tinchodestefanis1@gmail.com>
2018-09-17 14:18:58 -0300

    Activity pedido lista

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
-
 public class MainActivity extends AppCompatActivity {
 
     private Button btnNuevoPedido;
@@ -40,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         btnListaProductos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, PedidoActivity.class);
+                Intent i = new Intent(MainActivity.this, ListaProdActivity.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoActivity.java
@@ -1,29 +1,5 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+public class PedidoActivity {
 
-import java.util.ArrayList;
-import java.util.List;
-
-import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
-import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
-
-public class PedidoActivity extends AppCompatActivity {
-private Spinner spinner;
-private ArrayAdapter<Categoria> adapterCategoria;
-private ProductoRepository productoRepository;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.lista_productos);
-        productoRepository = new ProductoRepository();
-        spinner = (Spinner) findViewById(R.id.spinner);
-        //PROBAR LA LINEA DE ABAJO SINO AGREGARLE LA OTRA CON EL DROPDOWN
-        adapterCategoria = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,productoRepository.getCategorias());
-        spinner.setAdapter(adapterCategoria);
-    }
 }
