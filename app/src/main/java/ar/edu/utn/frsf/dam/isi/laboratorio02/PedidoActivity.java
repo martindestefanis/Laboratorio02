84a5c0fc81c6e9097764f9e52156ba6fb6efc0bf
Pedro Capdevila <pedro.capdevila@hotmail.com>
2018-09-18 13:58:38 -0300

    Continuación actividad nuevo pedido

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoActivity.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -29,6 +30,8 @@ public class PedidoActivity extends AppCompatActivity{
     private Bundle extras;
     private int extraID, extraCantidad;
     private Producto producto;
+    private Intent i;
+    private static final int CODIGO = 999;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -53,24 +56,29 @@ public class PedidoActivity extends AppCompatActivity{
         });
 
         unPedido = new Pedido();
+        lstPedidoItems = (ListView) findViewById(R.id.lstPedidoItems);
+        adapterLstPedidoItems = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_single_choice, unPedido.getDetalle());
+        lstPedidoItems.setAdapter(adapterLstPedidoItems);
 
+        btnPedidoAddProducto = (Button) findViewById(R.id.btnPedidoAddProducto);
         btnPedidoAddProducto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), ListaProdActivity.class);
-                startActivity(i);
-                extras = getIntent().getExtras();
-                extraID = extras.getInt("idProducto");
-                extraCantidad = extras.getInt("cantidad");
-                repositorioProducto = new ProductoRepository();
-                producto = repositorioProducto.buscarPorId(extraID);
-                unPedidoDetalle = new PedidoDetalle(extraCantidad, producto);
-
-                lstPedidoItems = (ListView) findViewById(R.id.lstPedidoItems);
-                adapterLstPedidoItems = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_single_choice, unPedido.getDetalle());
-                lstPedidoItems.setAdapter(adapterLstPedidoItems);
+                i = new Intent(getApplicationContext(), ListaProdActivity.class);
+                startActivityForResult(i, CODIGO);
             }
         });
 
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == CODIGO) {
+                    extraID = data.getExtras.getInt("idProducto");
+                    repositorioProducto = new ProductoRepository();
+                    producto = repositorioProducto.buscarPorId(extraID);
+                    extraCantidad = data.getExtras.getInt("cantidad");
+                    unPedidoDetalle = new PedidoDetalle(extraCantidad, producto);
+                }
+            }
+        }
     }
 }
