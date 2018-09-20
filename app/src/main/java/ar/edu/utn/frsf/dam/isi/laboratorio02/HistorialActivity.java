00bc8f314e80f8ac1a85fa8184b5796dd1cd4334
Victoria Perez <victoria_perez07@hotmail.com>
2018-09-20 14:17:06 -0300

    Continuación PedidoActivity y creación layout historial_pedido

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialActivity.java
@@ -0,0 +1,12 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HistorialActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.historial_pedidos);
+    }
+}
