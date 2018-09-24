a7f0bdbc3d27dcabe82465b369653eb901112ac5
martindestefanis <tinchodestefanis1@gmail.com>
2018-09-24 12:32:40 -0300

    Actividad 5 casi terminada (falla botón Cancelar)

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 
 public class HistorialActivity extends AppCompatActivity{
 
     private Button btnHistorialNuevo, btnhistorialMenu;
+    private ListView lstHistorialPedidos;
+    PedidoAdapter pedidoAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historial_pedidos);
 
-        /* ACA VA LO DEL LIST VIEW*/
+        lstHistorialPedidos = (ListView) findViewById(R.id.lstHistorialPedidos);
+        PedidoRepository pedidoRepository = new PedidoRepository();
+        pedidoAdapter = new PedidoAdapter(HistorialActivity.this,pedidoRepository.getLista());
+        lstHistorialPedidos.setAdapter(pedidoAdapter);
+
         btnHistorialNuevo = (Button) findViewById(R.id.btnHistorialNuevo);
         btnhistorialMenu = (Button) findViewById(R.id.btnhistorialMenu);
         btnHistorialNuevo.setOnClickListener(new View.OnClickListener() {
