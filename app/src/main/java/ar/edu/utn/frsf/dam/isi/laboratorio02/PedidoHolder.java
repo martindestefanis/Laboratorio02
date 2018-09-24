4a0249b29c4b95d7ca55b2f3097e6aa1666bdc11
martindestefanis <tinchodestefanis1@gmail.com>
2018-09-24 12:32:40 -0300

    Actividad 5 casi terminada (falla botón Cancelar)

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoHolder.java
@@ -0,0 +1,32 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+
+public class PedidoHolder extends AppCompatActivity{
+    Button btnCancelar;
+    TextView tvMailPedido;
+    TextView tvHoraEntrega;
+    TextView tvCantidadItems;
+    TextView tvPrecio;
+    TextView estado;
+    ImageView tipoEntrega;
+
+    PedidoHolder(View base) {
+        this.btnCancelar = (Button) base.findViewById(R.id.btnCancelar);
+        this.tvMailPedido = (TextView) base.findViewById(R.id.tvEmail);
+        this.tvHoraEntrega = (TextView) base.findViewById(R.id.tvFechaEntrega);
+        this.tvCantidadItems = (TextView) base.findViewById(R.id.tvItems);
+        this.tvPrecio = (TextView) base.findViewById(R.id.tvAPagar);
+        this.estado = (TextView) base.findViewById(R.id.lblEstado);
+        //Falta el imageView
+    }
+    }
+
+
